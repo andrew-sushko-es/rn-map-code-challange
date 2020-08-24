169411eb16f6e49c1d5deab45efc7c4f7bbcc13e
Andrew Sushko <andrew.sushko@effective-soft.com>
2020-08-25 01:01:35 +0300

    Add env config file, add possibility to change from env map for ios

--- src/screens/Map/index.js
+++ src/screens/Map/index.js
@@ -1,8 +1,10 @@
 import React, {useEffect, useState, useContext} from 'react';
-import {ThemeContext} from 'styled-components'
+import {ThemeContext} from 'styled-components';
+import {USE_IOS_NATIVE_MAP} from '@env';
 
+import {isIOS} from 'utils/common';
 import {getCurrentPosition} from 'utils/geolocation';
-import {showFlashMessage, MESSAGE_TYPES} from 'utils/flashMessages'
+import {showFlashMessage, MESSAGE_TYPES} from 'utils/flashMessages';
 
 import Map from 'components/Map';
 
@@ -13,21 +15,21 @@ const MapScreen = () => {
   const theme = useContext(ThemeContext);
 
   useEffect(() => {
-    getCurrentPosition((coords) => {
-      setCoordinates(coords);
-    }, (error) => {
-      showFlashMessage(
-        error.message,
-        MESSAGE_TYPES.ERROR,
-        theme,
-      )
-    });
+    getCurrentPosition(
+      (coords) => {
+        setCoordinates(coords);
+      },
+      (error) => {
+        showFlashMessage(error.message, MESSAGE_TYPES.ERROR, theme);
+      },
+    );
   }, []);
 
+  const isGoogleMapView = USE_IOS_NATIVE_MAP ? !isIOS() : true;
 
   return (
     <S.Container>
-      <Map userCoords={coordinates} />
+      <Map userCoords={coordinates} isGoogleMap={isGoogleMapView} />
     </S.Container>
   );
 };
