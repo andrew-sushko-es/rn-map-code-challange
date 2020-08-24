160bb3226a3e5f87e5de5126d508221ae47474d2
Andrew Sushko <andrew.sushko@effective-soft.com>
2020-08-25 00:43:57 +0300

    Integrate google maps for IOS

--- ios/rnMapsCodeChallange/AppDelegate.m
+++ ios/rnMapsCodeChallange/AppDelegate.m
@@ -11,6 +11,7 @@
 #import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
 #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
 #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
+#import <GoogleMaps/GoogleMaps.h>
 
 static void InitializeFlipper(UIApplication *application) {
   FlipperClient *client = [FlipperClient sharedClient];
@@ -31,6 +32,7 @@ static void InitializeFlipper(UIApplication *application) {
   InitializeFlipper(application);
 #endif
 
+  [GMSServices provideAPIKey:@"AIzaSyDO-I8EirTzbBFzrP-K8y8e8t8ro29JjK8"];
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"rnMapsCodeChallange"
