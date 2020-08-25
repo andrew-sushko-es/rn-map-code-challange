57276cb5fccf0e7a8cda21053aac86af4b673779
Andrew Sushko <andrew.sushko@effective-soft.com>
2020-08-25 19:33:29 +0300

    Format flashMessages file

--- src/utils/flashMessages.js
+++ src/utils/flashMessages.js
@@ -1,24 +1,24 @@
-import { showMessage } from "react-native-flash-message";
+import {showMessage} from 'react-native-flash-message';
 
 export const MESSAGE_TYPES = {
-    ERROR: 'error'
-}
+  ERROR: 'error',
+};
 
 const getColorsByType = (type, theme) => {
-    const colorsByType = {
-        [MESSAGE_TYPES.ERROR]: {
-            backgroundColor: theme.colors.error,
-            color: theme.colors.primaryText
-        },
-        default: {}
-    }
-    return colorsByType[type] || colorsByType.default
-}
+  const colorsByType = {
+    [MESSAGE_TYPES.ERROR]: {
+      backgroundColor: theme.colors.error,
+      color: theme.colors.primaryText,
+    },
+    default: {},
+  };
+  return colorsByType[type] || colorsByType.default;
+};
 
 export const showFlashMessage = (message, type, theme) => {
-    showMessage({
-        message,
-        type,
-        ...getColorsByType(type, theme)
-    })
-}
+  showMessage({
+    message,
+    type,
+    ...getColorsByType(type, theme),
+  });
+};
